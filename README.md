b14237e7eedc5e594b5885059c8307a3bd49abd6
sudopenguin14 <jhaa9718@gmail.com>
2026-04-16 07:53:56 +0000

    Guess the number

--- README.md
+++
@@ -1,8 +0,0 @@
-# Guess the Number 
-⚠️ IMPORTANT ⚠️
-
-Click on Releases and download the game for your Operating system.
-
-On Windows: Download the file "guessthenumber.exe" from releases. Double click to open it. You may get a security warning. This is fine as I am not a verified developer. Click on More info, then run anyway. Enjoy the game!
-
-On GNU/Linux Download the "guessthenumber-linux" file and open the terminal in the directory where you downloaded the game and type "./guessthenumber-linux" to launch it
---
+++ guessthenumber.py
@@ -0,0 +1,45 @@
+import random
+def game():
+  num= random.randint(1,100)
+  attempts=5
+  guess=0
+  print("I'm thinking of a number between 1 and 100")
+  print("You have 5 attempts. Can you guess it?")
+  while guess != num and attempts>0:
+    try:
+      print(f"\nAttempts remaining {attempts}")
+      guess=int(input("Enter your guess:"))
+      attempts -=1
+      if guess<num:
+        print("Too low! Try again")
+      elif guess>num:
+        print("Too high! Try again")
+      else:
+        print("Correct! The number was",num,"." "You've won the game")
+        return True
+    except ValueError:
+      print("Please enter a valid whole number")
+  print("\nGame Over!")
+  print("You have run out of attempts. You lost. The number was ",num)
+  return False
+while True:
+  game()
+  retry=input("\nDo you want to play again?[y/n]").lower().strip()
+  if retry!='y':
+    print("Thanks for playing. Goodbye")
+    break
+input(f"\nPress Enter to exit...")
+
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
